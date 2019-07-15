7865d062bc2a4db2415dcddb91c7f3870262aba1
shanetw4 <52422842+shanetw4@users.noreply.github.com>
2019-07-15 10:47:46 -0400

    Add files via upload

---
+++ python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/cern/cms_data_analysis/',1)[0])+'/cfipython/slc6_amd64_gcc472/cern/cms_data_analysis')
---
+++ python/demoanalyzer_cfi.py
@@ -0,0 +1,4 @@
+import FWCore.ParameterSet.Config as cms
+
+demo = cms.EDAnalyzer('cms_data_analysis'
+)
